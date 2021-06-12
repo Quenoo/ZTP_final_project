a9cf2c50e8688358f185391d31b6feff0494c42d
Quenoo <Miki10040@gmail.com>
2021-06-12 13:04:26 +0200

    add views

--- ztpfinal/foody/views.py
+++ ztpfinal/foody/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+
+from .serializers import *
+from .models import *
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = RecipeSerializer
+    # permission_classes = [permissions.IsAuthenticated]
