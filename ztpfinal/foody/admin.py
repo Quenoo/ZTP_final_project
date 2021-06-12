32abdcdd4ee293354921feb08196253146d50342
tzch96 <tzch96@gmail.com>
2021-06-12 12:32:08 +0200

    Register models manually because of error when doing this automatically

--- ztpfinal/foody/admin.py
+++ ztpfinal/foody/admin.py
@@ -1,7 +1,8 @@
-from django.apps import apps
 from django.contrib import admin
+from .models import Ingredient
+from .models import Recipe
+from .models import AppUser
 
-models = apps.get_models()
-
-for model in models:
-    admin.site.register(model)
+admin.site.register(Ingredient)
+admin.site.register(Recipe)
+admin.site.register(AppUser)
