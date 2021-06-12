214776b4147a9d86552377701bcffdbf8af42dcc
tzch96 <tzch96@gmail.com>
2021-06-12 12:15:02 +0200

    Initial database models configuration - Recipe, Ingredient, AppUser

--- ztpfinal/ztpfinal/settings.py
+++ ztpfinal/ztpfinal/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'foody.apps.FoodyConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
